3cff4b079d95d295f3812c35b1e5dffb66b94368
song-eun <dmswo546@naver.com>
2022-06-30 20:25:19 +0900

    chat service

---
+++ README.md
@@ -0,0 +1,7 @@
+# Noom
+
+Zoom Clone using NodeJS
+
+추가할 기능
+[x] 로그인
+[x] 방 번호로 입력해서 들어가고 주최자가 승인하기
--- src/server.js
+++ src/server.js
@@ -1,14 +1,29 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
-import { redirect } from "express/lib/response";
 
 const app = express();
+const port = 3000;
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/views");
 
 app.use("/public", express.static(__dirname + "/public"));
 
+//route 만들기
 app.get("/", (req, res) => res.render("home"));
 app.get("/*", (req, res) => res.redirect("/"));
-const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const handleListen = () => console.log(`Listening on http://localhost:${port}`);
+
+//app.listen(3000, handleListen);
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+//socket: 연결된 브라우저
+wss.on("connection", (socket) => {
+  console.log("Connected to Browser");
+  socket.on("close", () => console.log("Disconnected frome Browser"));
+  socket.send("hello!!!");
+});
+server.listen(3000, handleListen);
