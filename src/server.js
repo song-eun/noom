e382a2018758acfede5c72de3c7de5c24bf53c05
song-eun <dmswo546@naver.com>
2022-07-08 14:43:10 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -21,8 +21,8 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-  socket.on("enter_room", (msg, done) => {
-    console.log(msg);
+  socket.on("enter_room", (roomName, done) => {
+    console.log(roomName);
     setTimeout(() => {
       done();
     }, 10000);
